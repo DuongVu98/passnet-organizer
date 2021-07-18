970cacb8d7ef1d365bab73099b172309d87d5776
DuongVu98 <duongvutung1998@gmail.com>
2021-07-18 15:04:56 +0700

    first commit

---
+++ src/main/java/com/cseiu/passnetorganizer/PassnetOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.cseiu.passnetorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassnetOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassnetOrganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cseiu/passnetorganizer/PassnetOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cseiu.passnetorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassnetOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
